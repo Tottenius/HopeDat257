e10acae23797458b6edf06b13f3af54e2ad057d6
Tor Clementz <clementz@student.chalmers.se>
2020-09-14 13:43:18 +0200

    Javafxknappar inlagda. Buggar ut när man försöker lägga till kod

--- Code/src/controller/TestController.java
+++ Code/src/controller/TestController.java
@@ -1,5 +1,16 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+
 public class TestController {
+
+    @FXML
+    Button button1;
+
+    private void button1Clicked(){
+        System.out.println("Scroll scroll");
+    }
 
 }
