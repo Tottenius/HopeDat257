e487e633fb1c06df6e68e12d5084120c431ae86f
anton <antonskebaber@gmail.com>
2020-09-18 22:42:41 +0200

    Improved GUI

--- Code/src/controller/TestController.java
+++ Code/src/controller/TestController.java
@@ -1,17 +1,40 @@
 package controller;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
 
-public class TestController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TestController implements Initializable {
 
     @FXML
     private TextField breakfastTextField;
 
     @FXML
-    private void breakfastTextFieldAction(){
+    private void breakfastTextFieldAction() {
         System.out.println(this.breakfastTextField.getText());
     }
 
+
+    @FXML
+    TreeView <String> treeviewID;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        TreeItem<String> root = new TreeItem<>("Root");
+
+        TreeItem<String> nodeA = new TreeItem<>("Node A");
+        TreeItem<String> nodeB = new TreeItem<>("Node B");
+        TreeItem<String> nodeC = new TreeItem<>("Node C");
+
+        root.getChildren().addAll(nodeA,nodeB,nodeC);
+        treeviewID.setRoot(root);
+
+    }
 }
