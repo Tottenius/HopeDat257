10e10a6647b6b721c0bb7822cb5ab4c68a69b8b7
anton <antonskebaber@gmail.com>
2020-09-18 22:41:41 +0200

    Improved GUI

--- Code/src/main/Main.java
+++ Code/src/main/Main.java
@@ -1,6 +1,8 @@
 package main;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -14,17 +16,27 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         //Parent root = FXMLLoader.load(getClass().getResource("/viewer/sample.fxml"));
 
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/viewer/sample.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/viewer/sample2.fxml"));
         Parent root = loader.load();
 
         primaryStage.setTitle("Hope");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+
+    }
+
+    //Handle events such as button clicks etc...
+    public void handle(ActionEvent event){
+
+
+
+
     }
 
 
     public static void main(String[] args) {
         System.out.println("Test");
+        //Method that starts the program as a JavaFx application
         launch(args);
     }
 }
