74d3f864bfaabd912f8250f183ae5f1fdb518895
Pettsson12 <emil.pettsson@hotmail.com>
2020-10-01 13:00:41 +0200

    En klass med enums för matlistorna

---
+++ Code/src/model/FoodList.java
@@ -0,0 +1,13 @@
+package model;
+
+public class FoodList {
+
+    private enum FoodListEnum {
+        Meat, Spices, Carbs
+    }
+
+    private enum FoodListEnumMeat {
+        Beaf, Chicken, Lamb
+    }
+
+}
