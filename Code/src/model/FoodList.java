8bbf1dd4bdeef24ec9bedf05bb9f35c4a051cb40
Pettsson12 <emil.pettsson@hotmail.com>
2020-10-01 13:10:19 +0200

    Lade till lite enums med några keys

--- Code/src/model/FoodList.java
+++ Code/src/model/FoodList.java
@@ -1,5 +1,9 @@
 package model;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class FoodList {
 
     private enum FoodListEnum {
@@ -8,6 +12,24 @@ public class FoodList {
 
     private enum FoodListEnumMeat {
         Beaf, Chicken, Lamb
+    }
+
+    private Map<Integer, FoodListEnum> FoodListNames;
+    private Map<Double, FoodListEnumMeat> FoodListNamesMeat;
+
+    private void initFoodNames() {
+        FoodListNames = new HashMap<>();
+
+        FoodListNames.put(1, FoodListEnum.Meat);
+        FoodListNames.put(2, FoodListEnum.Spices);
+        FoodListNames.put(3, FoodListEnum.Carbs);
+    }
+
+    private void initFoodListNamesMeat() {
+        FoodListNamesMeat = new HashMap<>();
+
+        FoodListNamesMeat.put(0.0144, FoodListEnumMeat.Beaf);
+        FoodListNamesMeat.put(0.0025, FoodListEnumMeat.Chicken);
     }
 
 }
