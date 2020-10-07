705fbce4a7ce339d724901e52b2786cc68a2707a
carlhandersson <carl_andersson_99@hotmail.com>
2020-10-07 12:14:30 +0200

    Har tillsammans med Jonathan fixat så att en extern databas kan användas istället för en intern

--- Code/src/controller/LoginViewController.java
+++ Code/src/controller/LoginViewController.java
@@ -9,9 +9,11 @@ import javafx.scene.control.TextField;
 import main.RunMain;
 import model.UserData;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.MalformedURLException;
-import java.net.URL;
+import java.net.*;
 import java.util.ResourceBundle;
 
 public class LoginViewController implements Initializable {
@@ -31,6 +33,18 @@ public class LoginViewController implements Initializable {
     @FXML
     private TextField passwordTextField;
 
+    // Anropar contactServer för att fråga servern om login uppgifterna stämmer och loggar isf in
+    @FXML
+    private void loginClick() throws IOException {
+        String message = "login " + userTextField.getText() + " " + passwordTextField.getText();
+        String response = contactServer(message);
+        if(response.equals("true")) {
+            MainViewController.setUserData(new UserData(this.userTextField.getText()));
+        } else if(response.equals("false")) {
+            this.passwordTextField.setText("");
+        }
+    }
+    /*
     @FXML
     private void loginClick() throws IOException {
         System.out.println("Klickat på login");
@@ -46,13 +60,26 @@ public class LoginViewController implements Initializable {
             this.passwordTextField.setText("");
         }
     }
+    */
 
+     */
     //When user presses register button, the selected name and password is stored in the database.
 
     @FXML
     private void registerClick(){
         System.out.println("Klickat på register");
         MainViewController.c.register(1,userTextField.getText(), passwordTextField.getText());
+    }
+
+    // Skickar ett meddelande (message) till servern och returnerar ett svar från servern
+    private String contactServer(String message) throws IOException {
+        Socket socket = new Socket("217.209.131.86", 9999);
+        DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+        DataInputStream input = new DataInputStream(socket.getInputStream());
+        output.writeUTF(message);
+        String response = input.readUTF();
+        socket.close();
+        return response;
     }
 
     private MainViewController loadRootController()  {
---
+++ Code/src/sql/Server.java
@@ -0,0 +1,75 @@
+package sql;
+
+import java.io.*;
+import java.net.*;
+import java.sql.*;
+import java.util.*;
+
+public class Server {
+
+    private Connection sqlConnection;
+    ServerSocket serverSocket;
+    Socket socket;
+
+    public static void main(String[] args) throws Exception {
+        Server server = new Server();
+        server.init();
+        server.requestHandler();
+    }
+
+    // Initierar nödvändigheter för postgresql
+    private void init() throws Exception {
+        Class.forName("org.postgresql.Driver");
+        Properties props = new Properties();
+        props.setProperty("user", "postgres");
+        props.setProperty("password", "postgres");
+        sqlConnection = DriverManager.getConnection("jdbc:postgresql://localhost/postgres", props);
+    }
+
+    // Anropar receiver för att ta emot ett meddelande och kallar på aktuell metod för att hantera meddelandet
+    private void requestHandler() throws Exception {
+        while(true) {
+            String[] request = receiver();
+            switch (Objects.requireNonNull(request)[0]) {
+                case "login" -> login(request);
+                case "register" -> register(request);
+            }
+            serverSocket.close();
+        }
+    }
+
+    // Tar emot ett meddelande och return:ar innehållet som är uppdelat med mellanrum i en string array
+    // tex om meddelandet är "login Calle Password5" blir arrayen: array[0] = login, array[1] = Calle och array[2] = Password5
+    private String[] receiver() throws Exception {
+        serverSocket = new ServerSocket(9999);
+        socket = serverSocket.accept();
+        DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+        String message = dataInputStream.readUTF();
+        return message.split("\\s");
+    }
+
+    // Skickar tillbaka ett meddelande "message" till klienten som kontaktat servern
+    private void sender(String message) throws IOException {
+        DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+        dataOutputStream.writeUTF(message);
+    }
+
+    // Kollar om användarnamnet och lösenordet stämmer för en registrerad användare och anropar sender för att skicka tillbaka true eller false till klienten
+    private void login(String[] loginInfo) throws Exception {
+        PreparedStatement preparedStatement = sqlConnection.prepareStatement( "SELECT *  FROM Users WHERE name = ? AND password = ? ");
+        preparedStatement.setString(1, loginInfo[1]);
+        preparedStatement.setString(2, loginInfo[2]);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        if(resultSet.next()) {
+            sender("true");
+        } else {
+            sender("false");
+        }
+    }
+
+    private void register(String[] requests) {
+
+
+        System.out.println("registrera");
+    }
+}
