6f3ef7a3b414e0102f6c9f629fddc6b6b7c4f163
cjal95 <joalon@student.chalmers.se>
2020-10-08 14:08:33 +0200

    More food

--- Code/src/model/FoodPackage/FoodsEnum.java
+++ Code/src/model/FoodPackage/FoodsEnum.java
@@ -2,7 +2,11 @@ package model.FoodPackage;
 
 public enum FoodsEnum {
 
-    Beaf(0.0144), Chicken(0.0025), Lamb(0.0095), Pork(0.01);
+    Beef(0.0481), Pork(0.0042), Chicken(0.00206), Lamb(0.0454),
+    Milk(0.0011), Cream(0.006), Butter(0.0127), Cheese(0.0097),
+    Fish(0.0024), Pasta(0.0012), Bread(0.0008), Rice(0.0023),
+    Potatoes(0.0002), Carrots(0.0002), Tomatoes(0.0015), Lettuce(0.0003),
+    Apples(0.0003), Bananas(0.0006);
 
     private double em;
 
