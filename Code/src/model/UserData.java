622d9547c2c62a199079662a7bf97c71dc7db922
Tor Clementz <clementz@student.chalmers.se>
2020-10-22 12:12:54 +0200

    Städat upp UserData som grupp

--- Code/src/model/UserData.java
+++ Code/src/model/UserData.java
@@ -8,6 +8,7 @@ import java.util.Map;
 
 /**
  * Class handles the data of a user
+ *
  */
 public class UserData {
 
@@ -20,21 +21,35 @@ public class UserData {
 
     // Logged in?
     private boolean loggedIn = false;
+    // Map with the date as a key and a list with foods for that date.
+    private Map<String, List<Foods>> userData = new HashMap<>();
 
-    public Map<String, List<Foods>> userData = new HashMap<>();
-
-
+    /**
+     * This method adds a food to a certain date
+     * @param date The date that food is being added to
+     * @param food The food that is added
+     */
     public void addToUserData(String date, Foods food) {
+        // Create a temp list for the day
         List<Foods> todayList;
         if (!userData.containsKey(date)) {
+            // If there is nothing on todays date, make a new list for the date
             todayList = new ArrayList<>();
         } else {
+            // else get the list that exists on the date
             todayList = userData.get(date);
         }
+        // Add the food to the list
         todayList.add(food);
+        // Put the list back in the map on the right date
         userData.put(date , todayList);
     }
 
+    /**
+     *
+     * @param date The date
+     * @return Returns the emissions of the day as a double
+     */
     public double getEmissions(String date) {
         // Get today's list of foods
         List<Foods> todayList = this.userData.get(date);
@@ -47,17 +62,34 @@ public class UserData {
         return emissions;
     }
 
+    /**
+     * Returns the map with emissions for the user
+     * @return Returns the map with emissions for the user
+     */
     public Map<String, List<Foods>> getUserData() {
         return this.userData;
     }
 
+    /**
+     * Returns the users name
+     * @return Returns the users name
+     */
     public String getUser(){
         return this.user;
     }
 
+    /**
+     * Returns if the user is logged in or not
+     * @return  Returns if the user is logged in or not
+     */
     public boolean getLoggedIn(){
         return loggedIn;
     }
+
+    /**
+     * Sets the users logged in status
+     * @param b If true the user will be set to logged in
+     */
     public void setLoggedIn(boolean b){
         this.loggedIn = b;
     }
